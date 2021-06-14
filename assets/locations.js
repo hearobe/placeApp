5d76ffa1386c553c0eb40f8880cd0a86a1962535
hearobe <tan.yl1210@gmail.com>
2021-06-14 23:16:10 +0800

    retrieve and render data from locations file

--- assets/locations.js
+++ assets/locations.js
@@ -1,1 +1,39 @@
-export const locations = [{ name: "Gallop Kranji Farm Resort", address: "" }];
+export const locations = [
+	{
+		name: "Gallop Kranji Farm Resort",
+		imageUri:
+			"https://media-cdn.tripadvisor.com/media/photo-s/17/2e/79/46/gallop-kranji-farm-resort.jpg",
+		image: "../assets/kranjifarm.jpg",
+		address: "10 Neo Tiew Lane 2, S718813",
+		hours: "Monday - Sunday, 8am - 9pm",
+		type: "attraction",
+		price: "",
+		shortDesc: "Farmstay Resort",
+		longDesc:
+			"Gallop Kranji Farm Resort is a countryside destination located in Kranji, the north-west region of Singapore. Our Resort provides a local farm stay experience.",
+		rating: 4,
+	},
+	{
+		name: "Ah Tee Ko Ko Mee",
+		imageUri: "https://farm1.staticflickr.com/794/27170823678_31daab6b5b_h.jpg",
+		image: "../assets/kokomee.jpg",
+		address: "#01-100 Gourmet Street, Blk 27 Kelantan Court, S200027",
+		hours: "Monday -Saturday, 5.30am-1.30pm",
+		type: "food",
+		price: "",
+		shortDesc: "Authentic Ko Ko Mee",
+		longDesc:
+			"Sim Ah Tee, 73, has been a hawker for over 50 years. He started out with a pushcart in Jalan Besar hawking ‘ko ko mee’, fishball noodles which got its name from the seller hitting bamboo with a stick to produce a ‘kok kok’ sound to attract customers. ",
+		rating: 5,
+	},
+	{
+		name: "",
+		address: "",
+		hours: "",
+		type: "",
+		price: "",
+		shortDesc: "",
+		longDesc: "",
+		rating: 4,
+	},
+];
